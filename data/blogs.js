c96bab5d3a99d727e1b31120aedb3be175dba8e5
Ayushjaisgit <ayushcodez@gmail.com>
2022-10-13 17:43:26 +0530

    repo created

---
+++ data/blogs.js
@@ -0,0 +1,37 @@
+ // This is not a json file its just and array of objects
+ 
+ blogs = [
+    {
+        title: "how to get started with python",
+        content: "This is content",
+        slug:"python-learn"
+    },
+    {
+        title: "how to get started with js",
+        content: "This is content js",
+        slug:"js-learn"
+    },
+    {
+        title: "how to get started with R",
+        content: "This is content R",
+        slug:"R-learn"
+    },
+    {
+        title: "how to get started with Django",
+        content: "This is content Django",
+        slug:"django-learn"
+    },
+    {
+        title: "how to get started with CSS",
+        content: "This is content CSS",
+        slug:"css-learn"
+    },
+    {
+        title: "how to get started with Html",
+        content: "This is content htmls",
+        slug:"html-learn"
+    },
+]
+
+
+module.exports = blogs
